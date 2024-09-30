9839022817496b7d1e4cd235561fe44b025e8ef8
shivamnayak201019 <nayakshivam1@gmail.com>
2024-09-30 21:55:10 +0530

    created react structure and pages

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+
+export default function App() {
+  return <BrowserRouter> 
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/sign-in' element={<Signin/>}/>
+    <Route path='/sign-up' element={<Signup/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+   
+
+ 
+  </Routes>
+  
+  
+  
+  </BrowserRouter>;
+}
